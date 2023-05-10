533852a8411797852eee08bfaec27d767b6be51f
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-05-10 08:33:05 -0500

    Change::ZapatoPage.dart

--- my_app/lib/screens/ShoesApp/ZapatoPage.dart
+++ my_app/lib/screens/ShoesApp/ZapatoPage.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, empty_statements
+// ignore_for_file: prefer_const_constructors, empty_statements, prefer_const_literals_to_create_immutables, unused_element
 import 'package:flutter/material.dart';
+import '../../constants/ShoesApp/colors.dart';
 
 class ZapatoPage extends StatelessWidget {
   const ZapatoPage({super.key});
@@ -8,9 +9,17 @@ class ZapatoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To do app'),
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: _customAppBar(),
       ),
       body: Text('Hair cut'),
+    );
+  }
+
+  Widget _customAppBar() {
+    return Row(
+      children: [],
     );
   }
 }
