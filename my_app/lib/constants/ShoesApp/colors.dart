22bbe4d01a7b2195234972949142a5449af307d2
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-05-10 08:47:13 -0500

    Change::colors.dart

--- my_app/lib/constants/ShoesApp/colors.dart
+++ my_app/lib/constants/ShoesApp/colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 const Color tdBGColor = Color(0xFFFFFFFF);
 const Color tdRed = Color(0xFFDA4040);
-const Color tdBGColor = Color(0x000000);
-const Color tdBGColor = Color(0x2C7FE0);
-const Color tdBGColor = Color(0xFFE600);
-const Color tdBGColor = Color(0xD9D9D9);
-const Color tdBGColor = Color(0xEBEBEB);
+const Color tdBlack = Color(0xFF000000);
+const Color tdBlue = Color(0xFF2C7FE0);
+const Color tdYellow = Color(0xFFFFE600);
+const Color tdGray = Color(0xFFD9D9D9);
+const Color tdLigthGray = Color(0xFFEBEBEB);
