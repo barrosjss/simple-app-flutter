e5249184ea820c3d2a8e4b6b9eb12ae4cade5626
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-05-10 08:43:04 -0500

    colores

--- my_app/lib/constants/ShoesApp/colors.dart
+++ my_app/lib/constants/ShoesApp/colors.dart
@@ -2,3 +2,8 @@ import 'package:flutter/material.dart';
 
 const Color tdBGColor = Color(0xFFFFFFFF);
 const Color tdRed = Color(0xFFDA4040);
+const Color tdBGColor = Color(0x000000);
+const Color tdBGColor = Color(0x2C7FE0);
+const Color tdBGColor = Color(0xFFE600);
+const Color tdBGColor = Color(0xD9D9D9);
+const Color tdBGColor = Color(0xEBEBEB);
