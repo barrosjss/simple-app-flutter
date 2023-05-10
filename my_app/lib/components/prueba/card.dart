f36f8a9e7de8e6948329535a9ad775254cf4e7ff
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-05-10 09:55:20 -0500

    tarjeta

---
+++ my_app/lib/components/prueba/card.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/helpers/transform/transform.dart';
+import 'package:flutterapp/helpers/svg/svg.dart';
+
+/* Rectangle Rectangle 1*/
+
+class GeneratedRectangle1Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 240.0,
+      height: 228.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(6.0),
+        child: Container(
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+    );
+  }
+}
+
+
+/* Rectangle Nike-logo 1*/
+
+class GeneratedNikelogo1Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 64.0,
+      height: 48.0,
+      child: ClipRRect(
+        borderRadius: BorderRadius.zero,
+        child: Image.asset(
+          "assets/images/9b9c83300e6bdf2d5a169a6ccbc18d8e1c4ea7de.png",
+          color: null,
+          fit: BoxFit.cover,
+          width: 64.0,
+          height: 48.0,
+          colorBlendMode: BlendMode.dstATop,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/* Rectangle shoe 1*/
+
+class GeneratedShoe1Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TransformHelper.rotate(
+        a: 0.87,
+        b: -0.50,
+        c: 0.50,
+        d: 0.87,
+        child: Container(
+          width: 189.60736083984375,
+          height: 143.40956115722656,
+          child: ClipRRect(
+            borderRadius: BorderRadius.zero,
+            child: Image.asset(
+              "assets/images/73644af092139a7025a64995e32c7ca710c8ecdf.png",
+              color: null,
+              fit: BoxFit.cover,
+              width: 189.60736083984375,
+              height: 143.40956115722656,
+              colorBlendMode: BlendMode.dstATop,
+            ),
+          ),
+        ));
+  }
+}
+
+/* Vector Vector*/
+class GeneratedVectorWidget1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 22.853260040283203,
+      height: 22.853300094604492,
+      child: SvgWidget(painters: [
+        SvgPathPainter.fill()
+          ..addPath(
+              'M18.0349 16.4441L22.8533 21.2614L21.2614 22.8533L16.4442 18.0349C14.6518 19.4718 12.4223 20.2533 10.125 20.25C4.536 20.25 0 15.714 0 10.125C0 4.536 4.536 0 10.125 0C15.714 0 20.25 4.536 20.25 10.125C20.2533 12.4223 19.4718 14.6517 18.0349 16.4441ZM15.7782 15.6094C17.2059 14.1411 18.0033 12.173 18 10.125C18 5.7735 14.4754 2.25 10.125 2.25C5.7735 2.25 2.25 5.7735 2.25 10.125C2.25 14.4754 5.7735 18 10.125 18C12.173 18.0032 14.1412 17.2059 15.6094 15.7781L15.7782 15.6094Z')
+          ..color = Color.fromARGB(255, 138, 138, 155),
+      ]),
+    );
+  }
+}
+
+/* Text Nike Air Max 270*/
+class GeneratedNikeAirMax270Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '''Nike Air Max 270''',
+      overflow: TextOverflow.visible,
+      textAlign: TextAlign.left,
+      style: TextStyle(
+        height: 1.36181640625,
+        fontSize: 14.0,
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        color: Color.fromARGB(255, 0, 0, 0),
+
+        /* letterSpacing: 0.28, */
+      ),
+    );
+  }
+}
+
+
+/* Star Star 1*/
+
+class GeneratedStar1Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12.0,
+      height: 12.0,
+      child: SvgWidget(painters: [
+        SvgPathPainter.fill()
+          ..addPath(
+              'M6 0L7.34708 4.1459L11.7063 4.1459L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459L4.65292 4.1459L6 0Z')
+          ..color = Color.fromARGB(255, 255, 229, 0),
+      ]),
+    );
+  }
+}
+
+/* Star Star 2*/
+
+class GeneratedStar2Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12.0,
+      height: 12.0,
+      child: SvgWidget(painters: [
+        SvgPathPainter.fill()
+          ..addPath(
+              'M6 0L7.34708 4.1459L11.7063 4.1459L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459L4.65292 4.1459L6 0Z')
+          ..color = Color.fromARGB(255, 255, 229, 0),
+      ]),
+    );
+  }
+}
+
+/* Star Star 3*/
+
+class GeneratedStar3Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12.0,
+      height: 12.0,
+      child: SvgWidget(painters: [
+        SvgPathPainter.fill()
+          ..addPath(
+              'M6 0L7.34708 4.1459L11.7063 4.1459L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459L4.65292 4.1459L6 0Z')
+          ..color = Color.fromARGB(255, 255, 229, 0),
+      ]),
+    );
+  }
+}
+
+/* Star Star 4*/
+
+class GeneratedStar4Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12.0,
+      height: 12.0,
+      child: SvgWidget(painters: [
+        SvgPathPainter.fill()
+          ..addPath(
+              'M6 0L7.34708 4.1459L11.7063 4.1459L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459L4.65292 4.1459L6 0Z')
+          ..color = Color.fromARGB(255, 255, 229, 0),
+      ]),
+    );
+  }
+}
+
+/* Star Star 5*/
+
+class GeneratedStar5Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12.0,
+      height: 12.0,
+      child: SvgWidget(painters: [
+        SvgPathPainter.fill()
+          ..addPath(
+              'M6 0L7.34708 4.1459L11.7063 4.1459L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459L4.65292 4.1459L6 0Z')
+          ..color = Color.fromARGB(255, 217, 217, 217),
+      ]),
+    );
+  }
+}
