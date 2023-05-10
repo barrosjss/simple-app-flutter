16972f0c343046a994c62fd08fd9adcee5db780f
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-05-10 09:25:30 -0500

    cambios

--- my_app/lib/constants/ShoesApp/colors.dart
+++ my_app/lib/constants/ShoesApp/colors.dart
@@ -5,5 +5,5 @@ const Color tdRed = Color(0xFFDA4040);
 const Color tdBlack = Color(0xFF000000);
 const Color tdBlue = Color(0xFF2C7FE0);
 const Color tdYellow = Color(0xFFFFE600);
-const Color tdGray = Color(0xFFFBFBFD);
 const Color tdLigthGray = Color(0xFFEBEBEB);
+
